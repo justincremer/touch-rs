2d51b888b97a589d6489ea319662cd207f4bd48e
jacremer <jacremer@live.com>
2021-07-13 21:06:45 -0700

    fixes cross platform stdio issue with writeln macro

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate filetime;
 use filetime::{set_file_times, FileTime};
 use std::env;
 use std::fs::File;
-use std::io::{stderr, stdout};
+use std::io::{stderr, stdout, Write};
 use std::path::Path;
 use std::process::exit;
 use std::time::SystemTime;
@@ -26,6 +26,10 @@ OPTIONS
 "#; /* @MANEND */
 
 fn main() {
+    // TODO: change use of stdio for compatibility with winapi
+    // (example 1): Err(e) => stderr.write(format!("some error {}", e).as_bytes()),
+    // (example 2): if let Err(e) = stdout.write(b"This is an example") { stderr.write(e.as_bytes()); };
+
     let stdout = stdout();
     let mut stdout = stdout.lock();
     let mut stderr = stderr();
@@ -33,12 +37,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
         0 | 1 => {
-            let _ = writeln!(stderr, "{}", "Please provide an argument");
+            let _ = stderr.write(b"Please provide an argument");
             exit(1);
         }
         _ => match args[1].as_str() {
             "-h" | "--help" => {
-                let _ = writeln!(stdout, "{}", MAN_PAGE);
+                if let Err(e) = stdout.write(MAN_PAGE.as_bytes()) {
+                    let _ = stderr.write(format!("{}", e).as_bytes());
+                };
                 exit(0);
             }
             _ => {
@@ -47,11 +53,11 @@ fn main() {
                     if Path::new(&arg).is_file() {
                         let time = FileTime::from_system_time(SystemTime::now());
                         if let Err(e) = set_file_times(&arg, time, time) {
-                            let _ = writeln!(stderr, "{}", e);
+                            let _ = stderr.write(format!("{}", e).as_bytes());
                         }
                     } else {
                         if let Err(e) = File::create(&arg) {
-                            let _ = writeln!(stderr, "{}", e);
+                            let _ = stderr.write(format!("{}", e).as_bytes());
                         }
                     }
                 }
